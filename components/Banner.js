44b668bcdbc49377e47225a23b7ee08aa0536293
db360 <da.b@hotmail.es>
2021-11-14 14:07:27 +0100

    fetching

--- components/Banner.js
+++ components/Banner.js
@@ -6,8 +6,12 @@ const Banner = () => {
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
-        objectFit="contain"
+        objectFit="cover"
       />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg xl:text-xl">Not Sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full cursor-pointer font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">I'm Flexible</button>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -10,11 +10,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    {/* HEADER */}
       <Header />
-    {/* BANNER */}
       <Banner />
 
+      <main className="max-w-7xl mx-auto sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* pull data from the server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+
+// TELLS NEXTJS THIS IS SERVER SIDE RENDERING
+export async function getStaticProps() {
+  //JSON objetos con img, location, distance
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
